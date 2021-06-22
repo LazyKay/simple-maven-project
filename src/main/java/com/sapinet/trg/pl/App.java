6c87fe83dc309703a7e47066c281d05d3590fa89
Kishor Chand <kishorec15@gmail.com>
2021-06-22 14:49:35 +0530

    simple maven project created

---
+++ src/main/java/com/sapinet/trg/pl/App.java
@@ -0,0 +1,13 @@
+package com.sapinet.trg.pl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
